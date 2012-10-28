65b091a51d712fc117a5eba3346f42d6b2a3e30e
smallfish <xiaoyu.chen@aliyun-inc.com>
2012-10-28 17:54:27 +0800

    support stats/list/delete

--- README.md
+++ README.md
@@ -6,33 +6,57 @@
     server {
         location /test {
             content_by_lua '
+
                 local beanstalkd = require 'resty.beanstalkd'
 
                 -- new and connect
-                local beansd, err = beanstalkd:new()
-                if not beansd then
+                local bean, err = beanstalkd:new()
+                if not bean then
                     ngx.say("failed to init beanstalkd:", err)
                     return
                 end
-                ngx.say("init ok")
 
-                local ok, err = beansd:connect()
+                local ok, err = bean:connect()
                 if not ok then
                     ngx.say("failed to connect beanstalkd:", err)
                     return
                 end
-                ngx.say("connect ok")
 
-                -- put message to default tube
-                local ok, err = beansd:put("hello")
+                -- use tube
+                local ok, err = bean:use("smallfish")
+                if not ok then
+                    ngx.say("failed to use tube:", err)
+                    return
+                end
+                ngx.say("use tube ok:")
+
+                -- put job
+                local ok, err = bean:put("hello")
                 if not ok then
                     ngx.say("failed to put hello:", err)
                     return
                 end
-                ngx.say("put ok:", ok)
+                ngx.say("put ok, id:", ok)
+
+                -- delete job
+                local ok, err = bean:delete(10)
+                if ok == nil then
+                    ngx.say("failed to delete:", err)
+                    return
+                elseif ok == false then
+                    ngx.say("failed to delete:", err)
+                elseif ok then
+                    ngx.say("delete ok")
+                end
+
+                local ok, err = bean:list_tubes()
+                ngx.say("list_tubes:\r\n", ok)
+
+                local ok, err = bean:stats_tube("smallfish")
+                ngx.say("stats_tube:\r\n", ok)
 
                 -- close
-                beansd:close()
-            ';
+                bean:close()
+           ';
         }
     }
--- lib/resty/beanstalkd.lua
+++ lib/resty/beanstalkd.lua
@@ -17,7 +17,6 @@ function new(self)
     if not sock then
         return nil, err
     end
-
     return setmetatable({sock = sock}, mt)
 end
 
@@ -26,7 +25,6 @@ function set_timeout(self, timeout)
     if not sock then
         return nil, "not initialized"
     end
-
     return sock:settimeout(timeout)
 end
 
@@ -35,40 +33,121 @@ function connect(self, host, port, ...)
     if not sock then
         return nil, "not initialized"
     end
-
     host = host or "127.0.0.1"
     port = port or 11300
-
     return sock:connect(host, port, ...)
 end
 
-function put(self, body, priority, delay, ttr)
+function use(self, tube)
     local sock = self.sock
     if not sock then
         return nil, "not initialized"
     end
-
-    priority = priority or 2 ^ 31
-    delay    = delay or 0
-    ttr      = ttr or 120
-
-    local cmd = {"put", " ", priority, " ", delay, " ", ttr, " ", strlen(body), "\r\n", body, "\r\n"}
+    local cmd = {"use", " ", tube, "\r\n"}
     local bytes, err = sock:send(tabconcat(cmd))
     if not bytes then
-        return nil, "failed to send command:" .. err
+        return nil, "failed to use tube:" .. err
     end
-
     local line, err = sock:receive()
     if not line then
-        return nil, "failed to receive line:" .. err
+        return nil, "failed to use tube:" .. err
     end
+    return line
+end
 
+function put(self, body, pri, delay, ttr)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+    pri   = pri or 2 ^ 32
+    delay = delay or 0
+    ttr   = ttr or 120
+    local cmd = {"put", " ", pri, " ", delay, " ", ttr, " ", strlen(body), "\r\n", body, "\r\n"}
+    local bytes, err = sock:send(tabconcat(cmd))
+    if not bytes then
+        return nil, "failed to put:" .. err
+    end
+    local line, err = sock:receive()
+    if not line then
+        return nil, "failed to put:" .. err
+    end
     local id = strmatch(line, "^INSERTED (%d+)$")
-    if not id then
-        return nil, "failed to get insert id"
+    if id then
+        return id, line
     end
+    local id = strmatch(line, "^BURIED (%d+)$")
+    if id then
+        return id, line
+    end
+    return nil, line
+end
 
-    return id, nil
+function delete(self, id)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+    local cmd = {"delete", " ", id, "\r\n"}
+    local bytes, err = sock:send(tabconcat(cmd))
+    if not bytes then
+        return nil, "failed to delete:" .. err
+    end
+    local line, err = sock:receive()
+    if not line then
+        return nil, "failed to delete:" .. err
+    end
+    if line == "DELETED" then
+        return true, line
+    end
+    return false, line
+end
+
+function list_tubes(self)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+    local bytes, err = sock:send("list-tubes\r\n")
+    if not bytes then
+        return nil, "failed to list-tubes:" .. err
+    end
+    local line, err = sock:receive()
+    if not line then
+        return nil, "failed to list-tubes:" .. err
+    end
+    local size = strmatch(line, "^OK (%d+)$")
+    if not size then
+        return nil, "failed to list-tubes:" .. err
+    end
+    local line, err = sock:receive(size)
+    if not line then
+        return nil, "failed to list-tubes:" .. err
+    end
+    return line, nil
+end
+
+function stats_tube(self, name)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+    local cmd = {"stats-tube", " ", name, "\r\n"}
+    local bytes, err = sock:send(tabconcat(cmd))
+    if not bytes then
+        return nil, "failed to stats-tube:" .. err
+    end
+    local line, err = sock:receive()
+    local line, err = sock:receive()
+    if not line then
+        return nil, "failed to stats-tube:" .. err
+    end
+    local size = strmatch(line, "^OK (%d+)$")
+    if size then
+        local line, err = sock:receive(size)
+        return line, nil
+    end
+    return line, nil
 end
 
 function close(self)
@@ -76,7 +155,6 @@ function close(self)
     if not sock then
         return nil, "not initialized"
     end
-
     return sock:close()
 end
 
